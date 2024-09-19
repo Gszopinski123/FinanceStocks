12a917f9e90ed42dc708463a2030d2c5b32d1509
gszopin <szopinski00@gmail.com>
2024-09-18 22:04:43 -0400

    starting over

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default function myApp() {
+    return (
+        <p>Hello World! Part2</p>
+    )
+}
--- my-app/types.d.ts
+++
@@ -1,6 +0,0 @@
-type User = {
-        "userId": number,
-        "id": number,
-        "title": string,
-        "completed": boolean
-}
