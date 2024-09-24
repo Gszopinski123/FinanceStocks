ffda2acb89ed372a06f9c64021508f4e5436f4fc
gszopin <szopinski00@gmail.com>
2024-09-23 22:13:28 -0400

    added search for certain tickers

--- routes/search.js
+++ routes/search.js
@@ -2,12 +2,26 @@ const express = require('express')
 const router = express.Router()
 
 router.use(express.urlencoded({ extended: true })); 
-router.get('/',(req,res)=> {
+router.get('/',async(req,res)=> {
     console.log(req.query.search)
     if (req.query.search) {
+        searchTicker= `${req.query.search}`
+        var url =`https://api.polygon.io/v2/aggs/ticker/${req.query.search}/range/1/day/2023-01-09/2023-01-09?apiKey=8ukN3Z4dvAETRsn6s_UVpmYrce76oiOX`
+        try {
+            const response = await fetch(url)
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const json = await response.json();
+            console.log(json)
+        } catch (error) {
+            console.log(error)
+        }
         res.render("index", {text: "We Found Something!"})
+    } else {
+        res.render("search",{text:"This is the search page!",})
     }
-    res.render("search",{text:"This is the search page!",})
+    
 })
 
 module.exports = router
