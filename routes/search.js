d301c774849645988c92bd5743f8284be64afcaf
gszopin <szopinski00@gmail.com>
2024-09-23 23:55:15 -0400

    added feedback after search

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,7 @@ const router = express.Router()
 router.use(express.urlencoded({ extended: true })); 
 router.get('/',async(req,res)=> {
     console.log(req.query.search)
+    let text
     if (req.query.search) {
         searchTicker= `${req.query.search}`
         var url =`https://api.polygon.io/v2/aggs/ticker/${req.query.search}/range/1/day/2023-01-09/2023-01-09?apiKey=8ukN3Z4dvAETRsn6s_UVpmYrce76oiOX`
@@ -14,10 +15,15 @@ router.get('/',async(req,res)=> {
             }
             const json = await response.json();
             console.log(json)
+            if (json.queryCount >= 1) {
+                text = `There was ${json.queryCount} stock/bond with the ticker ${req.query.search}`
+            } else {
+                text = `There were no Stock/bond with the ticker ${req.query.search}`
+            }
         } catch (error) {
             console.log(error)
         }
-        res.render("index", {text: "We Found Something!"})
+        res.render("index", {text: text})
     } else {
         res.render("search",{text:"This is the search page!",})
     }
